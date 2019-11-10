1d8665b9c06630bfa24ccc4eecae978023124dfe
fred <fred.krampe.1@gmail.com>
2019-11-09 19:16:33 -0500

    jobs first pass

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
+
+class Blog(models.Model):
+
+     image = models.ImageField(upload_to='images/')
+     Title = models.CharField(max_length=100)
+     publicationDate = models.DateField(default=now)
+     body = models.TextField()
+
